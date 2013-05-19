87e6155c1c25d42b34f5c06d92b76f01e981b174
Ripul Thakur <ripul.thakur@accenture.com>
2013-05-19 11:01:26 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -1048,7 +1048,7 @@ $('#selTag').text("Tag");
 //$("#selTag").css("margin-left", "180px");
 $("#selTag").css("margin-top", "80px");
 alert(parseInt($("#tag_place").css("margin-top")))
-alert(parseInt($("#tag_place").css("margin-top"))+(-20)+'px');
+alert(parseInt($("#tag_place").css("margin-top"))+(40)+'px');
 $("#selTag").css("margin-top", parseInt($("#tag_place").css("margin-top"))+20+'px');
 $('#selTag').show();
 selected_cat = '';
