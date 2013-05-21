40f596d2b114aba0578b8155627a9f1a1493173b
Ripul Thakur <ripul.thakur@accenture.com>
2013-05-21 11:15:34 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -1096,7 +1096,6 @@ $("#tag_sel").hide();
 document.getElementById("add_tag").innerHTML=$('#tag_sel').val();
 $("#add_tag").css("margin-left", '250px');
 $("#add_tag").show();
-alert("Break point 1");
 getDocs(space_url);
 getFiles(space_url);
 getDiscussions(space_url);
