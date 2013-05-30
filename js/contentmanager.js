7be2ef5c8f2c953d3c0dac71c097e33bf550ae2b
Ripul Thakur <ripul.thakur@accenture.com>
2013-05-30 11:52:20 +0530

    commit changes

--- js/contentmanager.js
+++ js/contentmanager.js
@@ -5418,7 +5418,7 @@ uploadSelected=uploadSelected+'</table>';
 $("#change_selection_div").show();
 $("#selected_items").show();
 $("#start_uploading").show();
-$("#up_select_items_button").css("margin-left", parseInt($("#selected_items").css("margin-left"))+'px');
+$("#up_select_items_button").css("margin-left", parseInt($("#upload_selected_items").css("margin-left"))+'px');
 //$("#up_select_items_button").hide();
 $("#change_contents").hide();
 $("#start_copying_button").hide();
